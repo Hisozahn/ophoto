6ae51127635b9e42441e53a141c87a7e1fc58cd0
Igor.Romanov <romigor96@gmail.com>
2019-04-24 23:35:13 +0300

    fix rpc client concurrency issue

--- lib/rpc_client.py
+++ lib/rpc_client.py
@@ -13,7 +13,7 @@ class RPCClient(object):
         self.channel = None
         self.callback_queue = None
         self.exchange_name = None
-        self.waiter = asyncio.Event()
+        self.events = {}
 
     async def connect(self):
         self.transport, self.protocol = await aioamqp.connect()
@@ -31,26 +31,28 @@ class RPCClient(object):
         )
 
     async def on_response(self, channel, body, envelope, properties):
-        if self.corr_id == properties.correlation_id:
-            self.response = body
-
-        self.waiter.set()
+        if properties.correlation_id in self.events:
+            self.events[properties.correlation_id]['response'] = body
+            self.events[properties.correlation_id]['event'].set()
 
     async def call(self, routing_key, msg):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
+        response = None
+        corr_id = str(uuid.uuid4())
         await self.channel.basic_publish(
             payload=json.dumps(msg),
             exchange_name=self.exchange_name,
             routing_key=routing_key,
             properties={
                 'reply_to': self.callback_queue,
-                'correlation_id': self.corr_id,
+                'correlation_id': corr_id,
             },
         )
-        await self.waiter.wait()
-        self.waiter.clear()
+        event = asyncio.Event();
+        self.events[corr_id] = {'event': event, 'response': None}
+        await event.wait()
+        response = self.events[corr_id]['response']
+        del(self.events[corr_id])
 
 #        await self.protocol.close()
 
-        return json.loads(self.response)
+        return json.loads(response)
